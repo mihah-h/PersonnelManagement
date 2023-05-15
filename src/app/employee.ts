d2c09062735f057e6d40055b4ad11b0d61289003
PPnotGG <ave.ppgg@gmail.com>
2023-05-15 18:29:48 +0500

    improved post and get requests in server imitation

--- src/app/employee.ts
+++ src/app/employee.ts
@@ -2,16 +2,20 @@ export class Employee {
   name!: string
   surname!: string
   patronymic?: string
+  company!: string
   gender?: string
   email!: string
   phone?: number
-  project?: string
-  post?: string
-  salary?: number
+  project?: string[]
+  status?: string
+  post?: string[]
+  salary?: number[]
   education?: string
   birthDate?: Date
   interviewDate?: Date
   hiringDate?: Date
   firstWorkDay?: Date
+  appointmentDate?: Date[]
+  vacations?: Date[]
   lastTrainingDate?: Date
 }
