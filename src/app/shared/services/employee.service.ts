65fae31b6601380bff140f7532eb2c7c3dcbefed
DanRaySS <danilmux@gmail.com>
2023-06-01 22:52:22 +0500

    info_accordion_with_base

--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -1,6 +1,6 @@
-import {Injectable} from "@angular/core";
-import {EmployeesInformation} from "../interfaces/employee-interfaces";
-import {Observable} from "rxjs";
+import { Injectable } from "@angular/core";
+import { EmployeesInformation } from "../interfaces/employee-interfaces";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class EmployeeService {
@@ -12,7 +12,7 @@ export class EmployeeService {
           employees: [{
             photo: '',
             name: 'Vova',
-            surname: 'S',
+            surname: 'Smith ',
             patronymic: 'R',
             phoneNumber: '567',
             email: 'a@gmail.com',
@@ -29,31 +29,33 @@ export class EmployeeService {
             experience: '2',
             firstWorkingDayDate: '13.12.2021',
             vacations: ['10.10.2022-9.11.2022', '10.03.2022-9.04.2022'],
-            historyInCompany: [{project: 'abanking', position: 'junior', salary: '56000', date: '10.10.2021'},
-              {project: 'abanking', position: 'midl', salary: '120000', date: '07.06.2022'}]},
+            historyInCompany: [{ project: 'abanking', position: 'junior', salary: '56000', date: '10.10.2021' },
+            { project: 'abanking', position: 'midl', salary: '120000', date: '07.06.2022' }]
+          },
 
-            {
-              photo: '',
-              name: 'Vika',
-              surname: 'r',
-              patronymic: 't',
-              phoneNumber: '564654',
-              email: 'gg@gmail.com',
-              status: 'work',
-              project: 'paper',
-              position: 'midl',
-              salary: '160000',
-              gender: 'woman',
-              education: 'courses',
-              birthDate: '06.16.1996',
-              age: '27',
-              interviewDate: '11.14.2022',
-              employmentDate: '13.12.2021',
-              experience: '2',
-              firstWorkingDayDate: '13.12.2020',
-              vacations: ['10.10.2022-9.11.2022', '10.03.2022-9.04.2022'],
-              historyInCompany: [{project: 'abanking', position: 'junior', salary: '56000', date: '10.10.2021'},
-                {project: 'paper', position: 'midl', salary: '160000', date: '07.06.2022'}]}
+          {
+            photo: '',
+            name: 'Vika',
+            surname: 'r',
+            patronymic: 't',
+            phoneNumber: '564654',
+            email: 'gg@gmail.com',
+            status: 'work',
+            project: 'paper',
+            position: 'midl',
+            salary: '160000',
+            gender: 'woman',
+            education: 'courses',
+            birthDate: '06.16.1996',
+            age: '27',
+            interviewDate: '11.14.2022',
+            employmentDate: '13.12.2021',
+            experience: '2',
+            firstWorkingDayDate: '13.12.2020',
+            vacations: ['10.10.2022-9.11.2022', '10.03.2022-9.04.2022'],
+            historyInCompany: [{ project: 'abanking', position: 'junior', salary: '56000', date: '10.10.2021' },
+            { project: 'paper', position: 'midl', salary: '160000', date: '07.06.2022' }]
+          }
           ],
           options: [
             {
