8a5593fd879aade550ffbf5fc601721914bec2e7
DanRaySS <danilmux@gmail.com>
2023-06-02 22:22:18 +0500

    empty_custom_attributive-directive

---
+++ src/app/shared/directives/directives/attribute.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Input, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appAttribute]',
+  host: {
+    '(click)': 'onClick()',
+    '(focus)': 'focus()'
+  }
+})
+export class AttributeDirective {
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) { }
+  hover() {
+    this.el.nativeElement.style.color='#3B82F6'
+  }
+}
