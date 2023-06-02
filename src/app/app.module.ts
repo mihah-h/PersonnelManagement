1cd035de188704bb91f179be0d6ac9048115c3f9
DanRaySS <danilmux@gmail.com>
2023-06-03 00:31:00 +0500

    Revert "Update app.module.ts"
    This reverts commit 41be321ffbe0aa72b2feb868f2d8963472176bc2.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,15 +21,7 @@ import {EmployeeListItemComponent} from "./shared/components/employee-list-item/
 import {SharedModule} from "./shared/shared.module";
 import {PipesModule} from "./shared/pipes/pipes/pipes.module";
 import {DirectivesModule} from "./shared/directives/directives/directives.module";
-<<<<<<< Updated upstream
 import {GuardAuth} from "./shared/services/guard.auth";
-=======
-<<<<<<< HEAD
-import { AttributeDirective } from './shared/directives/directives/attribute.directive';
-=======
-import {GuardAuth} from "./shared/services/guard.auth";
->>>>>>> f5208b6ccafb9e9312aee26883324b33bb211aef
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -44,7 +36,6 @@ import {GuardAuth} from "./shared/services/guard.auth";
     AdminLayoutComponent,
     FiltrationFormComponent,
     EmployeeListItemComponent,
-    AttributeDirective,
   ],
   imports: [
     BrowserModule,
